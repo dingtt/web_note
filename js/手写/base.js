10bac64450a77f1e95b853f3c677215642b4afd8
dingdtt <1018977091@qq.com>
2020-08-10 17:59:11 +0800

    new create call apply bind

--- js/手写/base.js
+++ js/手写/base.js
@@ -1,7 +1,6 @@
 // todo  手写 call  apply  bind instanceof  深拷贝 浅拷贝
-// 高阶  promise axios  
+// 高阶  promise axios
 // 其他 webpack-loader webpack-plugin
-
 
 // 手写new
 const myNew = function () {
@@ -20,7 +19,6 @@ const myCreate = function (obj) {
   return new F()
 }
 
-
 // instanceof
 const myInstanceof = function (O, i) {
   const left = O.prorotype
@@ -37,20 +35,80 @@ const myInstanceof = function (O, i) {
 }
 
 // call
+Function.prototype.call2 = function (context) {
+  // 没有传参，及window
+  var context = context || window
+  // 调用call的函数
+  context.fn = this
+  // 函数参数
+  var argsArr = Array.prototype.slice.call(arguments, 1) // 这里按说不应该使用call，偷个懒
+  // var args = argsArr.slice(1, argsArr.length - 1)
+  var result = eval("context.fn(" + argsArr.join(",") + ")")
+  delete context.fn
+  return result
+}
+// 测试一下
+var value = 2
+var obj = {
+  value: 1,
+}
+function bar(name, age) {
+  console.log(this.value)
+  return {
+    value: this.value,
+    name: name,
+    age: age,
+  }
+}
+bar.call(null) // 2
+console.log(bar.call2(obj, "kevin", 18))
 
-// appaly
+// apply
+Function.prototype.apply = function (context, arr) {
+  var context = Object(context) || window
+  context.fn = this
 
+  var result
+  if (!arr) {
+    result = context.fn()
+  } else {
+    var args = []
+    for (var i = 0, len = arr.length; i < len; i++) {
+      args.push("arr[" + i + "]")
+    }
+    result = eval("context.fn(" + args + ")")
+  }
+
+  delete context.fn
+  return result
+}
 
 //bind
+// bind() 方法会创建一个新函数
+// bind() 的第一个参数将作为它运行时的 this，之后的一序列参数将会在传递的实参前传入作为它的参数。
+
+Object.prototype.bind2 = function (context) {
+  var args = Array.prototype.slice.call(arguments, 1)
+  var self = this
+  var FNOP = function () {}
+  var fbound = function () {
+    var bindArgs = Array.prototype.slice.call(arguments)
+    self.apply(this instanceof self ? this : context, args.concat(bindArgs))
+  }
+  FNOP.prototype = this.prorotype
+  fbound.prorotype = new FNOP()
+  // fbound.prototype = this.prototype;
+  return fbound
+}
 
 // 深拷贝与浅拷贝
 // 浅拷贝是创建一个新对象，这个对象有着原始对象属性值的一份精确拷贝。如果属性是基本类型，拷贝的就是基本类型的值，如果属性是引用类型，拷贝的就是内存地址 ，所以如果其中一个对象改变了这个地址，就会影响到另一个对象。
 // 浅 Object.assgin() lodash 展开运输算符 Array.prototype.concat() Array.prototype.slice()
-function shallowClone(obj){
+function shallowClone(obj) {
   let newObj = {}
-  for(let k in obj){
-    if(obj.hasOwnProperty(k)){
-       newObj[k] = obj[k]
+  for (let k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      newObj[k] = obj[k]
     }
   }
   return newObj
@@ -58,15 +116,15 @@ function shallowClone(obj){
 // 深拷贝是将一个对象从内存中完整的拷贝一份出来,从堆内存中开辟一个新的区域存放新对象,且修改新对象不会影响原对象。
 // JSON.parse(JSON.stringify()) loadsh query.extend() 手写递归
 function deepClone(obj) {
-   if(obj === null) return obj
-   if(obj instanceof Date) return new Date(obj)
-   if(obj instanceof RegExp) return new RegExp(obj) 
-   if(typeof obj !== 'object') return obj
-   const newObj = {}
-   for(let k in obj){
-     if(obj.hasOwnproperty(k)){
-       newObj[k] = deepClone(obj[k])
-     }
-   }
-   return newObj
+  if (obj === null) return obj
+  if (obj instanceof Date) return new Date(obj)
+  if (obj instanceof RegExp) return new RegExp(obj)
+  if (typeof obj !== "object") return obj
+  const newObj = {}
+  for (let k in obj) {
+    if (obj.hasOwnproperty(k)) {
+      newObj[k] = deepClone(obj[k])
+    }
+  }
+  return newObj
 }
